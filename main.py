84fa07fdfa930c1de0ef1bd3922f95cc4063efaa
MusangQim <musangqim03@gmail.com>
2026-08-26 18:02:15 +0800

    Update: Add function finding the device info with specific find using 'getprop' target on model, version, security and manufacturer, applying learning about dictionary [key: value] and how to extract it, and also handling error

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import platform
 import subprocess
 
 
+# note: using adb.exe by auto locating the file after put folder in the repo
 def get_adb_path():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     system_os = platform.system().lower()
@@ -19,6 +20,7 @@ def get_adb_path():
             pass
 
     return adb_path
+
 
 # note: using tuple to standardize into (status, data)
 def check_adb_connection():
@@ -47,10 +49,42 @@ def check_adb_connection():
         return ("ERROR", "Invalid ADB output format!")
 # --- authorization and status ---
     if status == "unauthorized":
-        return ("ERROR", "Phone unauthorize - check phone screen for allow prompt")
+        return ("ERROR", "Phone unauthorize - check phone screen "
+                "for allow prompt")
     if status != "device":
         return ("ERROR", f"Unknown device status: {status}")
     return ("OK", device_id)
+
+
+def get_device_info(device_id):
+    adb_path = get_adb_path()
+    if not adb_path or not os.path.exists(adb_path):
+        return ("ERROR", "'platform-tools' folder not found!\n"
+                "Please download SDK PlatformTools and put in the repo folder")
+# --- specific target ---
+    target_keys = {
+        "ro.product.model": "model",
+        "ro.build.version.release": "android_version",
+        "ro.build.version.security_patch": "security_patch",
+        "ro.product.manufacturer": "manufacturer",
+    }
+# --- find getprop
+    command = [adb_path, "-s", device_id, "shell", "getprop"]
+    try:
+        result = subprocess.run(command, capture_output=True, text=True)
+    except FileNotFoundError:
+        return ("ERROR", "No data")
+    lines_info = result.stdout.strip().splitlines()
+    device_info = {}
+    for line in lines_info:
+        if ": " in line:
+            key_raw, val_raw = line.split(": ", 1)
+            key = key_raw.strip("[]")
+            val = val_raw.strip("[]")
+            if key in target_keys:
+                readable_name = target_keys[key]
+                device_info[readable_name] = val
+    return ("OK", device_info)
 
 
 if __name__ == "__main__":
